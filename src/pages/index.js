a962f297a0cfa391bf99c3bbd8d141dfa7b87ea3
Muosvr <lujason2015@gmail.com>
2018-08-30 23:20:36 -0400

    intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to Jason&#39;s website</p>
-    <p>Now go build something great.</p>
+    <p>I am teaching myself coding so I may provide more value to the world!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
